80272d71ddd171f7411508dc7d1c4b52b272e28e
José Ramón López Guillén <jose.rlg27@gmail.com>
2022-12-14 21:27:43 +0100

    añadiendo directores & acl

--- models/videoclub.py
+++ models/videoclub.py
@@ -12,9 +12,35 @@ class pelicula(models.Model):
      autor_ids = fields.Many2many('res.partner',string='Autor')
      duracion = fields.Float('Duración en minutos')
      descripcion = fields.Text('Descripción')
+     director = fields.Many2one('videoclub.director')
 
      @api.onchange('duracion')
      def _comprobar_duracion(self):
          for record in self:
              if record.duracion and not(record.duracion > 39 and record.duracion < 181):
                 raise models.ValidationError('La duración debe estar comprendida entre 40 y 180 minutos.')
+
+
+class director(models.Model):
+    _name='videoclub.director'
+    _description ='Director de la pelicula'
+    nombre = fields.Char('Nombre')
+    apellido = fields.Char('Apellido')
+    peliculas = fields.One2many('videoclub.pelicula','director')
+
+    _rec_name='nombre'
+
+    @api.constrains('nombre')
+    def _check_name(self):
+        for r in self:
+            if len(r.nombre) < 2:
+                raise models.ValidationError('La longitud del nombre del director debe ser mayor de 2 caracteres')
+
+    # Not Working
+    #@api.one
+    #def name_get(self):
+    #    res = []
+    #    for r in self:
+    #        res.append ((r.nombre, r.apellido))
+    #    return res    
+
